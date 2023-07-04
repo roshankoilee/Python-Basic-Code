95dcd92946118096412369341a7c6c5d7114e23a
roshankoilee <138573123+roshankoilee@users.noreply.github.com>
2023-07-04 14:33:48 +0530

    Add files via upload

---
+++ Python Basic.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+S = 45
+Y = 54
+c = 45+54
+print(c)
+
+
+# In[3]:
+
+
+d = 67
+h = 98
+y = 67/98
+print(y)
+
+
+# In[4]:
+
+
+y = 59
+h = 634
+o = 59*634
+print(o)
+
+
+# In[5]:
+
+
+t = 89
+j = 32
+m = 89-32
+print(m)
+
+
+# In[6]:
+
+
+for i in range(100):
+    print(i)
+
+
+# In[7]:
+
+
+for i in range(10):
+    print(i)
+
+
+# In[9]:
+
+
+for i in range(0,11,2):
+    print(i)
+
+
+# In[12]:
+
+
+for i in range(100):
+    print(i)
+
+              
+
+
+# In[14]:
+
+
+for i in range(1,5):
+    print(i)
+
+
+# In[15]:
+
+
+for i in range(100):
+    print(i)
+
+
+# In[16]:
+
+
+for i in range(45):
+    print(i)
+
+              
+
+
+# In[17]:
+
+
+for i in range(300):
+    print(i)
+
+
+# In[18]:
+
+
+dipansh = 79
+rahul = 67
+taklu = 89
+jinnat = 65
+sum = 79+67+89+65
+print(sum)
+
+
+
+# In[19]:
+
+
+hello =  54
+yutube = 65
+gym = 54+65
+print(gym)
+
+
+# In[20]:
+
+
+for i in range(78):
+    print(i)
+
+
+# In[21]:
+
+
+for i in range(1,10,2):
+    print(i)
+
+
+# In[22]:
+
+
+for i in range(1,30,4):
+    print(i)
+
+              
+
+
+# In[26]:
+
+
+for i in range(0,10,2):
+    print(i)
+
+
+# In[28]:
+
+
+for i in range(1,17,2):
+    print(i)
+
+
+# In[29]:
+
+
+for i in range(100):
+    print(i)
+
+
+# In[34]:
+
+
+for i in range(0,10):
+    for j in range(0,i):
+        print("Taklya tuzi ", end = " ")
+    print(" ")
+
+
+# In[37]:
+
+
+for i in range(110):
+    for b in range(0,i):
+        print("javeriya ", end = " ")
+    print(" ")
+
+
+# In[41]:
+
+
+for i in range(100):
+    print(i)
+
+
+# In[45]:
+
+
+for i in range(0,100):
+    for r in range(0,i):
+        print("roshan" , end = " ")
+    print(" ")
+
+
+# In[49]:
+
+
+for i in range(10):
+    for g in range(i):
+        print("+",end = " ")
+    print(" ")        
+
+
+# In[50]:
+
+
+for i in range(19):
+    print(i)
+
+
+# In[55]:
+
+
+for i in range(12):
+    for c in range(i):
+        print("rahul", end = " ")
+    print(" ")
+
+              
+
+
+# In[59]:
+
+
+for i in range(45):
+    for d in range(i):
+        print("sum ", end = " ")
+    print(" ")    
+
+
+# In[60]:
+
+
+for i in range(9):
+    print(i)
+
+              
+    
+
+
+# In[63]:
+
+
+for i in range(20):
+    for d in range(i):
+        print("blooming ", end = " ")
+    print(" ")   
+
+
+# In[ ]:
+
+
+
+
